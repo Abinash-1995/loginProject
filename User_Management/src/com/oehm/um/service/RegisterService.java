66dc88322f90411e2dc5bf555aad8acfa1ad0e87
Abinash-1995 <65123893+Abinash-1995@users.noreply.github.com>
2020-05-11 11:24:43 +0530

    Update RegisterService.java

--- User_Management/src/com/oehm/um/service/RegisterService.java
+++ User_Management/src/com/oehm/um/service/RegisterService.java
@@ -1,10 +1,17 @@
 package com.oehm.um.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.oehm.um.dao.RegisterDao;
 import com.oehm.um.entity.Register;
 
 public class RegisterService {
+	@Autowired
+	private RegisterDao registerDao;
 	
 	public void saveRegisterData(Register register) {
+		registerDao.saveRegisterData(register);
+		
 		
 	}
 
